320623f6ce7fefd95529c8b87acb90d3cdd056c8
suzichenapk@gmail.com <suzichenapk@gmail.com>
2017-11-06 15:36:25 +0800

    更改 内容头部改为动态获取

--- src/app/content/content-hd/content-hd.component.ts
+++ src/app/content/content-hd/content-hd.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import "rxjs/add/operator/filter"
 
 @Component({
-  selector: 'admin-content-hd',
-  templateUrl: './content-hd.component.html',
-  styleUrls: ['./content-hd.component.css']
+    selector: 'admin-content-hd',
+    templateUrl: './content-hd.component.html',
+    styleUrls: ['./content-hd.component.css']
 })
 export class ContentHdComponent implements OnInit {
 
-  constructor() { }
+    private pageTitle:string = '首页';
+    private pageDes:string = '首页信息';
 
-  ngOnInit() {
-  }
+    constructor(
+        router: Router
+    ) { 
+        // 路由事件
+        router.events
+        // 过滤出导航结束事件
+            .filter(event => event instanceof NavigationEnd)
+            // 订阅所有此类事件
+            .subscribe((event:NavigationEnd) => {
+                // 执行回调
+                if (event.url === '/home') {
+                    this.pageTitle = '首页';
+                    this.pageDes = '首页信息';
+                } else if (event.url.startsWith('/stock')) {
+                    this.pageTitle = '股票管理';
+                    this.pageDes = '股票的增删改查';
+                }
+            })
+     }
+
+    ngOnInit() {
+    }
 
 }
