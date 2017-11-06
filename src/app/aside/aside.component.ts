8e9ca0e848f601ef1b32728eaecf42214f653425
suzichenapk@gmail.com <suzichenapk@gmail.com>
2017-11-06 15:02:35 +0800

    菜单列表改为动态生成

--- src/app/aside/aside.component.ts
+++ src/app/aside/aside.component.ts
@@ -8,14 +8,30 @@ import { Router } from "@angular/router";
 })
 export class AsideComponent implements OnInit {
 
+    public menus:Array<Menu>;
+    public currentMenuId:number;
+
     constructor(
         public router:Router
     ) { }
 
     ngOnInit() {
+        this.menus = [
+            new Menu(1, '首页', 'home'),
+            new Menu(2, '股票管理', 'stock')
+        ]
     }
 
-    nav(url:string) {
-        this.router.navigateByUrl(url);
+    nav(menu:Menu) {
+        this.router.navigateByUrl(menu.link);
+        this.currentMenuId = menu.id;
     }
+}
+
+export class Menu {
+    constructor(
+        public id: number,
+        public name: string,
+        public link: string
+    ) {}
 }
