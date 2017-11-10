02e450a0d5eacdb69df016604ffb835caed382ef
suzichenapk@gmail.com <suzichenapk@gmail.com>
2017-11-10 19:08:46 +0800

    add ratingChange

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,23 +1,43 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'admin-stars',
     templateUrl: './stars.component.html',
     styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges {
 
     @Input()
     private rating:number;
+
+    @Input()
+    private readOnly:boolean = true;
+
+    @Output()
+    private ratingChange:EventEmitter<number> = new EventEmitter();
+
 
     private stars:Array<boolean> = [];
 
     constructor() { }
 
     ngOnInit() {
-        for(let i = 0; i<=5; i++) {
+
+    }
+
+    ngOnChanges() {
+        this.stars = [];
+        for(let i = 1; i <= 5; i++) {
             this.stars.push( i > this.rating )
         }
+    }
+
+    changeStar(index:number) {
+        if (this.readOnly === true) {
+            return;
+        }
+        this.rating = index + 1;
+        this.ratingChange.emit(this.rating);
     }
 
 }
