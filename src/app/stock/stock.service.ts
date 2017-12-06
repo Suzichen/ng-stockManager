80996f7bc4b02a76a03d1b09e004b40cff6cc980
suzichenapk@gmail.com <suzichenapk@gmail.com>
2017-12-06 22:55:39 +0800

    test

--- src/app/stock/stock.service.ts
+++ src/app/stock/stock.service.ts
@@ -9,13 +9,7 @@ export class StockService {
         private http: HttpClient
     ) { }
 
-    private stocks:Stock[] = [
-        new Stock( 21, "第一支股票", 124324, 2, "没有描述", ["IT","互联网"] ),
-        new Stock( 12, "第二支股票", 543535, 3, "没有描述", ["金融"] ),
-        new Stock( 11, "第三支股票", 545654, 1, "没有描述", ["互联网","金融"] ),
-        new Stock( 5, "第四支股票", 865454, 4, "没有描述", ["医疗"] ),
-        new Stock( 3, "第五支股票", 123454, 3, "没有描述", ["互联网","教育"] )
-    ];
+    
 
     public getStocks(): Observable<any> {
         let stocks = this.http.get('/api/stock');
